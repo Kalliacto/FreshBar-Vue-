aee06e647ef321f39517a0fdd812199b44b71669
Karina Boronina <Gubashina@yandex.ru>
2024-06-17 13:32:13 +0300

    Валидация поля с номером телефона

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -17,17 +17,24 @@
                 <input class="order__input" type="text" name="name" required placeholder="Имя" />
             </label>
             <label class="order__label">
-                <input class="order__input" type="text" name="phone" required placeholder="Телефон" />
+                <input
+                    class="order__input"
+                    type="number"
+                    name="phone"
+                    required
+                    placeholder="Телефон"
+                    @input="updatePhone"
+                    :value="phone"
+                />
+                <p v-if="isValidPhone" class="order__info">Номер введен корректно!</p>
             </label>
             <button class="order__submit btn" type="submit">Заказать</button>
-            <!-- Валидация на ончейнже -->
         </form>
     </div>
     <div class="order" v-else>
         <h2 class="order__title text__gradient_min">Корзина</h2>
         <p>Очень жаль, что Ваша корзина пуста {{ '=(' }}</p>
         <p>Пройти за <a class="text-red" href="#goods" @click="store.closeModal">покупками</a></p>
-        <!-- TODO: Стилизовать до конца блок отсутствия товаров в корзине -->
     </div>
 </template>
 
@@ -36,9 +43,21 @@ import { useGoodsStore } from '../store/GoodsStore';
 import CartItem from '../components/CartItem.vue';
 import { computed } from 'vue';
 const store = useGoodsStore();
+import { ref, watch } from 'vue';
 
 const allCountCoctail = computed(() => store.cart.reduce((acc, current) => acc + current.count, 0));
 const totalPrice = computed(() => store.cart.reduce((acc, current) => acc + current.price, 0));
+const phone = ref('');
+const isValidPhone = ref(false);
+
+watch(phone, (newEmail) => {
+    const regex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+    isValidPhone.value = regex.test(newEmail);
+});
+
+const updatePhone = (e) => {
+    phone.value = e.target.value;
+};
 
 const sendOrder = (e) => {
     const data = Object.fromEntries(new FormData(e.target));
@@ -71,6 +90,7 @@ const sendOrder = (e) => {
 }
 .order__label {
     width: 100%;
+    position: relative;
 }
 .order__input {
     width: 100%;
@@ -84,5 +104,10 @@ const sendOrder = (e) => {
 .order__submit {
     justify-self: end;
     grid-column: 2/3;
+}
+.order__info {
+    position: absolute;
+    margin-top: 5px;
+    margin-left: 5px;
 }
 </style>
